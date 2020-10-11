2604c3aa8ccb8df881eda61c56a416ea26d802a9
SabraAbraham <61850985+SabraAbraham@users.noreply.github.com>
2020-10-11 19:54:56 -0400

    Add files via upload

---
+++ GradebookTester.java
@@ -0,0 +1,47 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class GradebookTester {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testAddScore() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSum() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMinimum() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFinalScore() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetScoreSize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented");
+	}
+
+}
