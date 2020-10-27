ec071f67909bb0791e4224c510a8bfa691c58cfd
SabraAbraham <61850985+SabraAbraham@users.noreply.github.com>
2020-10-27 15:15:09 -0400

    Add files via upload

--- GradebookTester.java
+++ GradebookTester.java
@@ -5,33 +5,50 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class GradebookTester {
+GradeBook g1;
+GradeBook g2;
 
 	@BeforeEach
 	void setUp() throws Exception {
+		g1 = new GradeBook(5);
+		g2 = new GradeBook(5);
+		g1.addScore(90.0);
+		g1.addScore(83.1);
+		g1.addScore(62.0);
+		
+		g2.addScore(73.7);
+		g2.addScore(89.3);
+		g2.addScore(91.2);
+		
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		g1 = null;
+		g2 = null;
+		
 	}
 
 	@Test
 	void testAddScore() {
-		fail("Not yet implemented");
+		
+		assertTrue(g1.toString().contentEquals("90.0 83.1 62.0"));
+		assertEquals(3,g1.getScoreSize(),5);
 	}
 
 	@Test
 	void testSum() {
-		fail("Not yet implemented");
+		assertEquals(235.1,g1.sum(),235.1);
 	}
 
 	@Test
 	void testMinimum() {
-		fail("Not yet implemented");
+		assertEquals(62.0,g1.minimum(),62.0);
 	}
 
 	@Test
 	void testFinalScore() {
-		fail("Not yet implemented");
+		assertEquals(173.1,g1.finalScore(),173.1);
 	}
 
 	@Test
